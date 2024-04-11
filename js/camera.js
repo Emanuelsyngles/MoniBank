83ccff8bab14003d285bf5e6a267c1b69fbe5ce5
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-11 15:35:08 -0300

    Implementando o reconhecimento facial.

---
+++ js/camera.js
@@ -0,0 +1,4 @@
+const botaoIniciarCamera = document.querySelector('[data-video-botao]');
+const campoCamera = decument.querySelector('[data-camera]');
+const video = document.querySelector('[data-video]');
+
